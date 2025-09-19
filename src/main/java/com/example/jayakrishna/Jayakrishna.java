53dc02ec1706b511e2308277597b522c9e93d729
manu-1252 <s.manojkumar447@gmail.com>
2025-09-19 14:48:47 +0530

    JAYAKRISHNAS0001 - Created HelloWorld Program

---
+++ src/main/java/com/example/jayakrishna/Jayakrishna.java
@@ -0,0 +1,7 @@
+package com.example.jayakrishna;
+
+public class Jayakrishna {
+    public static void main(String[] args) {
+        System.out.println("JAYAKRISHNAS0001 - Created HelloWorld Program");
+    }
+}
